b97ef94db6ff6a2faf0fb7ee0860b0709e189140
LDenninger <Luis0512@web.de>
2023-08-08 14:10:17 +0200

    Added config structure

---
+++ config/config.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+class Config(object):
+    RANDOM_SEED = 42
+
+    # Model Parameters
+
+
+    # Training Parameters
+
+
+    # Optimizer Parameters
+
+
+    # Data Parameters
