4a4cef25a5794fd29e48174c9270858eef8d19fa
LDenninger <Luis0512@web.de>
2023-08-19 13:22:56 +0200

    Added baseline loss and scheduler

---
+++ utils/losses.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from abc import abstractmethod
+
+class LossBase(nn.Module):
+
+    def __init__(self):
+        super(LossBase, self).__init__()
+    
+    @abstractmethod
+    def forward(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        pass
+
+
+class PerJointMSELoss(LossBase):
+
+    def __init__(self):
+        super(PerJointMSELoss, self).__init__()
+
+    def forward(self, output: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        loss = F.mse_loss(output, target, reduction='none') # mse loss between joints
+        loss = torch.sum(loss, dim=-1) # Sum over all joints
+        loss = torch.sum(loss, dim=-1) # sum over all timesteps
+        return torch.mean(loss) # average over the batch
+
---
+++ utils/schedulers.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+from abs import abstractmethod
+"""
+    Schedulers used in this project.
+
+"""
+
+
+class SchedulerBase(nn.Module):
+    """
+        Base class for the learning rate schedulers.
+    """
+    def __init__(self, optimizer: nn.Module) -> None:
+        super(SchedulerBase, self).__init__()
+        self.optimizer = optimizer
+        self.register_parameter('learning_rate', torch.zero(1))
+
+    def forward(self, step: int) -> None:
+        """
+            Forward function to update the learning rate for the next step.
+        """
+        self.compute_learning_rate(step)
+        self.update_learning_rate()
+        return
+
+    @abstractmethod
+    def compute_learning_rate(self, step: int) -> None:
+        """
+            Abstract method to be implemented by the schedulers.
+            Computes the learning rate for the next step.
+        """
+        pass
+
+    def update_learning_rate(self) -> None:
+        """
+            Updates the learning rate for all parameters groups at once.
+            If the parameter groups have separate learning rates, this functions need to be reimplemented.
+        """
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = self.learning_rate
+
+
+class SPLScheduler(SchedulerBase):
+
+    def __init__(self,
+                 optimizer: nn.Module,
+                  emb_size: int,
+                   warmup: int) -> None:
+        super(SPLScheduler, self).__init__(optimizer)
+        self.emb_size = emb_size
+        self.warmup = warmup
+
+    def compute_learning_rate(self, step: int) -> None:
+        self.learning_rate = (self.emb_size ** -0.5) * torch.min(step ** -0.5, step * self.warmup ** -1.5)
