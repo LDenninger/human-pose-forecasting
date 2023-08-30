Added rest of the models; fixed attention mask; fixed transformer return

src/models/attention.py
@@ -49,7 +49,7 @@ class AttentionBase(nn.Module):
         query, key, value = query.reshape(-1, shape[-2], shape[-1]), key.reshape(-1, shape[-2], shape[-1]), value.reshape(-1, shape[-2], shape[-1])
         attn = torch.bmm(query, torch.transpose(key, 1, 2)) * (self.token_dim ** -0.5) 
         if self.mask is not None:
-            attn = torch.einsum('bij,ij->bij', attn, self.mask)
+            attn += self.mask.unsqueeze(0)
         attn = torch.softmax(attn, dim=-1)
         out = torch.bmm(attn, value)
         out = out.view(*shape)
@@ -147,7 +147,7 @@ class TemporalAttention(AttentionBase):
         return out
 
     def set_mask(self) -> None:
-        self.mask = torch.tril(torch.ones(self.num_tokens, self.num_tokens), diagonal=0)
+        self.mask = torch.triu(torch.ones(self.num_tokens, self.num_tokens), diagonal=1) * 1e9
 
 class SpatialAttention(AttentionBase):
     def __init__(self,
@@ -206,4 +206,37 @@ class SpatialAttention(AttentionBase):
         out = torch.einsum('nik,bjnk->bjni', torch.transpose(W, -2, -1), x) # shape [batch_size*seq_len, num_joints, token_dim]
         out = out.view(*shape) # shape [batch_size, seq_len, num_joints, token_dim]
         return out
+
+class VanillaAttention(AttentionBase):
+
+    class SpatialAttention(AttentionBase):
+        def __init__(self,
+                    num_emb: int,
+                    num_tokens: int,
+                    token_dim: int,
+                        num_heads: int) -> None:
+            super().__init__(num_tokens, token_dim, num_heads)
+            self.num_emb = num_emb
+            self.register_parameter('W_query', nn.Parameter(torch.Tensor(num_emb, token_dim, token_dim)))
+            self.register_parameter('W_key', nn.Parameter(torch.Tensor(token_dim, token_dim)))
+            self.register_parameter('W_value', nn.Parameter(torch.Tensor(token_dim, token_dim)))
+            self.register_parameter('W_output', nn.Parameter(torch.Tensor(token_dim, token_dim)))
+            self._reset_parameters()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+            Apply temporal attention to the input tensor.
+
+            Input:
+                x [batch_size, seq_len, num_joints * emb_dim]
+        """
+        Q = self.multi_head_linear_embedding(x, self.W_query) # [batch_size,num_tokens, token_dim]
+        K = self.multi_head_linear_embedding(x, self.W_key)
+        V = self.multi_head_linear_embedding(x, self.W_value)
+
+        out = self.scaled_dot_product_attention(Q, K, V)
+        out = self.linear_embedding(out, self.W_output)
+        return out
     
+    def set_mask(self) -> None:
+        self.mask = torch.triu(torch.ones(self.num_tokens, self.num_tokens), diagonal=1) * 1e9

src/models/transformer.py
@@ -16,6 +16,102 @@ from .utils import PointWiseLinear
 
 #####===== Transformer Block ====#####
 
+class VanillaTransformer(nn.Module):
+    def __init__(
+                    self,
+                     emb_dim: int,
+                     ff_dim: int,
+                     num_emb: int,
+                     seq_len: int,
+                       temporal_heads: int,
+                       spatial_heads: int,
+                         temporal_dropout: float = 0.0,
+                         spatial_dropout: float =0.0,
+                         ff_dropout: float = 0.0,
+                          full_return: bool = False
+
+                          ):
+        super(VanillaTransformer, self).__init__()
+        # Meta Parameters
+        self.emb_dim = emb_dim
+        self.ff_dim = ff_dim
+        self.num_emb = num_emb
+        self.seq_len = seq_len
+        self.temporal_heads = temporal_heads
+        self.spatial_heads = spatial_heads
+        self.temporal_dropout = temporal_dropout
+        self.spatial_dropout = spatial_dropout
+        self.ff_dropout = ff_dropout
+
+        self.full_return = full_return
+        # Define used modules
+        self.vanillaAttention = VanillaTransformer(
+            num_emb=num_emb,
+            num_tokens=seq_len,
+            token_dim=emb_dim,
+            num_heads = temporal_heads
+        )
+        self.spatialAttention = SpatialAttention(
+            num_emb=num_emb,
+            num_tokens=num_emb,
+            token_dim=emb_dim,
+            num_heads = spatial_heads
+        )
+        self.layerNorm = nn.LayerNorm(emb_dim, eps=1e-6)
+        self.pointWiseFF = self._build_point_wise_ff()
+        self.temporalDropout = None
+        self.spatialDropout = None
+        self.ffDropout = None
+
+        if temporal_dropout !=0:
+            self.temporalDropout = nn.Dropout(p=temporal_dropout)
+        if spatial_dropout!=0:
+            self.spatialDropout = nn.Dropout(p=spatial_dropout)
+        if ff_dropout!=0:
+            self.ffDropout = nn.Dropout(p=ff_dropout)
+
+        
+
+    
+    def forward(self, x):
+        """
+            Forward function for the Spatial-Temporal Attention Block.
+
+            Inputs:
+                x: input tensor, shape: [batch_size, seq_len, num_joints, emb_dim]
+        """
+        # Compute spatial and temporal attention separately and update input
+        spatialAttentionOut = self.spatialAttention(x) # shape: [batch_size, num_joints, seq_len, emb_dim]
+        if self.spatialDropout is not None:
+            spatialAttentionOut = self.spatialDropout(spatialAttentionOut)
+        temporalAttentionOut = self.temporalAttention(x)
+        if self.temporalDropout is not None:
+            temporalAttentionOut = self.temporalDropout(temporalAttentionOut)
+        # Add spatial and temporal attention
+        attnOut = self.layerNorm(x + spatialAttentionOut) + self.layerNorm(x+temporalAttentionOut) # shape: [batch_size, num_joints, seq_len, emb_dim]
+        # Point-wise feed-forward layer (point-wise w.r.t. the joints)
+        # TODO: Implement this more efficiently by defining projection by hand
+        ffOut = self.pointWiseFF(attnOut)
+        if self.ffDropout is not None:
+            ffOut = self.ffDropout(ffOut)
+        ffOut = self.layerNorm(ffOut + attnOut)
+
+        if self.full_return:
+            return ffOut, temporalAttentionOut, spatialAttentionOut
+        
+        return ffOut
+        
+    def _build_point_wise_ff(self):
+        return nn.Sequential(
+            nn.Linear(self.emb_dim, self.ff_dim),
+            nn.ReLU(),
+            nn.Linear(self.ff_dim, self.emb_dim),
+        )
+    
+    def extra_repr(self) -> str:
+        return f'emb_size={self.emb_dim}, num_emb={self.num_emb}, temporal_heads={self.temporal_heads}, spatial_heads={self.spatial_heads}, temporal_dropout={self.temporal_dropout}, spatial_dropout={self.spatial_dropout}, ff_dropout={self.ff_dropout}'
+    
+
 class SpatioTemporalTransformer(nn.Module):
     """
         Module representing a single Spatial-Temporal Attention Block following: https://arxiv.org/pdf/2004.08692.pdf
@@ -100,10 +196,213 @@ class SpatioTemporalTransformer(nn.Module):
         ffOut = self.layerNorm(ffOut + attnOut)
 
         if self.full_return:
-            return attnOut, temporalAttentionOut, spatialAttentionOut
+            return ffOut, temporalAttentionOut, spatialAttentionOut
         
-        return attnOut
+        return ffOut
         
+    def _build_point_wise_ff(self):
+        return nn.Sequential(
+            nn.Linear(self.emb_dim, self.ff_dim),
+            nn.ReLU(),
+            nn.Linear(self.ff_dim, self.emb_dim),
+        )
+    
+    def extra_repr(self) -> str:
+        return f'emb_size={self.emb_dim}, num_emb={self.num_emb}, temporal_heads={self.temporal_heads}, spatial_heads={self.spatial_heads}, temporal_dropout={self.temporal_dropout}, spatial_dropout={self.spatial_dropout}, ff_dropout={self.ff_dropout}'
+    
+class SeqSpatioTemporalTransformer(nn.Module):
+
+    def __init__(
+                    self,
+                     emb_dim: int,
+                     ff_dim: int,
+                     num_emb: int,
+                     seq_len: int,
+                       temporal_heads: int,
+                       spatial_heads: int,
+                         temporal_dropout: float = 0.0,
+                         spatial_dropout: float =0.0,
+                         ff_dropout: float = 0.0,
+                          full_return: bool = False
+
+                          ):
+        super(SpatioTemporalTransformer, self).__init__()
+        # Meta Parameters
+        self.emb_dim = emb_dim
+        self.ff_dim = ff_dim
+        self.num_emb = num_emb
+        self.seq_len = seq_len
+        self.temporal_heads = temporal_heads
+        self.spatial_heads = spatial_heads
+        self.temporal_dropout = temporal_dropout
+        self.spatial_dropout = spatial_dropout
+        self.ff_dropout = ff_dropout
+
+        self.full_return = full_return
+        # Define used modules
+        self.spatialAttention = SpatialAttention(
+            num_emb=num_emb,
+            num_tokens=num_emb,
+            token_dim=emb_dim,
+            num_heads = spatial_heads
+        )
+        self.spatialPointWiseFF = self._build_point_wise_ff()
+
+        self.temporalAttention = TemporalAttention(
+            num_emb=num_emb,
+            num_tokens=seq_len,
+            token_dim=emb_dim,
+            num_heads = temporal_heads
+        )
+        self.temporalPointWiseFF = self._build_point_wise_ff()
+
+
+        self.layerNorm = nn.LayerNorm(emb_dim, eps=1e-6)
+        self.temporalDropout = None
+        self.spatialDropout = None
+        self.ffDropout = None
+
+        if temporal_dropout !=0:
+            self.temporalDropout = nn.Dropout(p=temporal_dropout)
+        if spatial_dropout!=0:
+            self.spatialDropout = nn.Dropout(p=spatial_dropout)
+        if ff_dropout!=0:
+            self.ffDropout = nn.Dropout(p=ff_dropout)
+
+        
+        return
+    
+    def forward(self, x):
+        """
+            Forward function for the Spatial-Temporal Attention Block.
+
+            Inputs:
+                x: input tensor, shape: [batch_size, seq_len, num_joints, emb_dim]
+        """
+        # Compute spatial and temporal attention separately and update input
+        spatialAttentionOut = self.spatialAttention(x) # shape: [batch_size, num_joints, seq_len, emb_dim]
+        if self.spatialDropout is not None:
+            spatialAttentionOut = self.spatialDropout(spatialAttentionOut)
+        spatialOut = self.spatialPointWiseFF(spatialAttentionOut)
+        if self.ffDropout is not None:
+            spatialOut = self.ffDropout(spatialOut)
+        spatialOut = self.LayerNorm(spatialOut + x)
+
+        temporalAttentionOut = self.temporalAttention(spatialOut)
+        if self.temporalDropout is not None:
+            temporalAttentionOut = self.temporalDropout(temporalAttentionOut)
+        temporalOut = self.temporalPointWiseFF(temporalAttentionOut)
+        if self.ffDropout is not None:
+            temporalOut = self.ffDropout(temporalOut)
+        temporalOut = self.LayerNorm(temporalOut + spatialOut)
+        
+        if self.full_return:
+            return temporalOut, temporalAttentionOut, spatialAttentionOut
+        
+        return temporalOut
+    
+    def _build_point_wise_ff(self):
+        return nn.Sequential(
+            nn.Linear(self.emb_dim, self.ff_dim),
+            nn.ReLU(),
+            nn.Linear(self.ff_dim, self.emb_dim),
+        )
+    
+    def extra_repr(self) -> str:
+        return f'emb_size={self.emb_dim}, num_emb={self.num_emb}, temporal_heads={self.temporal_heads}, spatial_heads={self.spatial_heads}, temporal_dropout={self.temporal_dropout}, spatial_dropout={self.spatial_dropout}, ff_dropout={self.ff_dropout}'
+    
+
+class SeqTemporalSpatialTransformer(SpatioTemporalTransformer):
+
+    def __init__(
+                    self,
+                     emb_dim: int,
+                     ff_dim: int,
+                     num_emb: int,
+                     seq_len: int,
+                       temporal_heads: int,
+                       spatial_heads: int,
+                         temporal_dropout: float = 0.0,
+                         spatial_dropout: float =0.0,
+                         ff_dropout: float = 0.0,
+                          full_return: bool = False
+
+                          ):
+        super(SpatioTemporalTransformer, self).__init__()
+        # Meta Parameters
+        self.emb_dim = emb_dim
+        self.ff_dim = ff_dim
+        self.num_emb = num_emb
+        self.seq_len = seq_len
+        self.temporal_heads = temporal_heads
+        self.spatial_heads = spatial_heads
+        self.temporal_dropout = temporal_dropout
+        self.spatial_dropout = spatial_dropout
+        self.ff_dropout = ff_dropout
+
+        self.full_return = full_return
+        # Define used modules
+        self.temporalAttention = TemporalAttention(
+            num_emb=num_emb,
+            num_tokens=seq_len,
+            token_dim=emb_dim,
+            num_heads = temporal_heads
+        )
+        self.temporalPointWiseFF = self._build_point_wise_ff()
+
+        self.spatialAttention = SpatialAttention(
+            num_emb=num_emb,
+            num_tokens=num_emb,
+            token_dim=emb_dim,
+            num_heads = spatial_heads
+        )
+        self.spatialPointWiseFF = self._build_point_wise_ff()
+
+
+        self.layerNorm = nn.LayerNorm(emb_dim, eps=1e-6)
+        self.temporalDropout = None
+        self.spatialDropout = None
+        self.ffDropout = None
+
+        if temporal_dropout !=0:
+            self.temporalDropout = nn.Dropout(p=temporal_dropout)
+        if spatial_dropout!=0:
+            self.spatialDropout = nn.Dropout(p=spatial_dropout)
+        if ff_dropout!=0:
+            self.ffDropout = nn.Dropout(p=ff_dropout)
+
+        
+        return
+    
+    def forward(self, x):
+        """
+            Forward function for the Spatial-Temporal Attention Block.
+
+            Inputs:
+                x: input tensor, shape: [batch_size, seq_len, num_joints, emb_dim]
+        """
+        # Compute spatial and temporal attention separately and update input
+        temporalAttentionOut = self.temporalAttention(x)
+        if self.temporalDropout is not None:
+            temporalAttentionOut = self.temporalDropout(temporalAttentionOut)
+        temporalOut = self.temporalPointWiseFF(temporalAttentionOut)
+        if self.ffDropout is not None:
+            temporalOut = self.ffDropout(temporalOut)
+        temporalOut = self.LayerNorm(temporalOut + x)
+
+        spatialAttentionOut = self.spatialAttention(temporalOut) # shape: [batch_size, num_joints, seq_len, emb_dim]
+        if self.spatialDropout is not None:
+            spatialAttentionOut = self.spatialDropout(spatialAttentionOut)
+        spatialOut = self.spatialPointWiseFF(spatialAttentionOut)
+        if self.ffDropout is not None:
+            spatialOut = self.ffDropout(spatialOut)
+        spatialOut = self.LayerNorm(spatialOut + temporalOut)
+        
+        if self.full_return:
+            return spatialOut, temporalAttentionOut, spatialAttentionOut
+        
+        return spatialOut
+    
     def _build_point_wise_ff(self):
         return nn.Sequential(
             nn.Linear(self.emb_dim, self.ff_dim),